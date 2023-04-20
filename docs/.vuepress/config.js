d7b13445574146c438407706b999a223afd0bc4b
aim467 <709544711@qq.com>
2023-04-20 23:40:51 +0800

    fix: add friend link

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -45,6 +45,14 @@ module.exports = {
                 location: 2,     // 在导航栏菜单中所占的位置，默认3
                 text: 'Tag'      // 默认文案 “标签”
             },
-        }
+        },
+        friendLink: [
+            {
+                title: 'Dedsec2z Blog',
+                desc: '',
+                logo: "https://p3-tt-ipv6.byteimg.com/origin/pgc-image/10d1842054d0455d894e5289d3d81c91",
+                link: 'https://dedsec2z.top/'
+            }
+        ]
     }
 }
